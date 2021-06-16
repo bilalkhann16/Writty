a1389ce872a555a71e614dfd5a098cb5950a0418
Bilal K <lalbikhan2014@gmail.com>
2021-06-17 00:28:45 +0500

    View Functions Added

--- flask-project/app.py
+++ flask-project/app.py
@@ -16,13 +16,11 @@ database = "todolist")
 
 cursor = connection.cursor()
 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bilalk:bilal123@localhost/todolist'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'everything-starts-with-hello-world'
 
 db = SQLAlchemy(app)
-
 
 class users(db.Model):
     user_name= db.Column(db.String(50), nullable = False,primary_key=True)
@@ -70,6 +68,7 @@ def login():
         password = request.form["password"]
         print ('USERName----',user_name,password)
         query = """Select * from users where user_name= '%s'  """ %user_name
+        print ('q', query)
         out = cursor.execute(query)
         context_record = cursor.fetchall()
         recordKeys = []
@@ -84,11 +83,49 @@ def login():
             session['user_name'] = user_name
             is_login = True
             #return 'work fine!'
-            return redirect(url_for("helloworld"))
+            return redirect(url_for("task"))
             #return render_template('taskhome.html') #Page with two options, View and ADD the taks.
 
         return render_template('login.html')    
     return render_template('login.html')
+
+@app.route('/task')   #Viewtask. add task.
+def task():
+    return render_template('task_page.html')
+
+@app.route('/add-task-page')   #Adding the tasks page!
+def add_task():
+    return render_template('add_tasks_page.html')
+
+@app.route('/adding_task', methods=['POST','GET'])   #Adding the tasks page!
+def adding_task():
+    if "user_name" in session and request.method == "POST":
+        task_description = request.form["task_description"]
+        tag = request.form["tag"]
+        due_date = request.form["datee"]
+
+        thistuple = (session["user_name"],tag,due_date,task_description)
+        queryy = "Insert into tasks values" + str(thistuple) + ";"
+        querryy = """ %s """%queryy 
+        cursor.execute(querryy)
+        connection.commit()
+        return render_template('task_page.html')
+    else:
+        return 'Something went wrong in adding task page!'
+
+@app.route('/view-task', methods=['GET'])
+def view_tasks():
+    if "user_name" in session: 
+        query = """ select task_description,date,tag from tasks where user_name= '%s';  """ %session['user_name']                            #query to view the all available tasks with no specific conditions!
+        cursor.execute(query)
+        records = cursor.fetchall()
+        print(records)
+        if (len(records)== 0): #if no tasks found
+            return "No tasks found!"
+        connection.commit()
+        return 'Done'
+    else:
+        return 'Something went wrong in VIEW tasks'
 
 @app.route("/helloworld")
 def helloworld():
@@ -104,15 +141,6 @@ def logout():
     session.pop("user_name", None)
     return redirect(url_for("login"))
 
-
-# @app.route("/addtask", methods=['POST','GET'])
-# def add_task():
-    
-#     sql_query =""" select * from users where user_name = 'bilalkhann' """
-#     out = cursor.execute(sql_query)
-#     context_record = cursor.fetchall()
-#     print (context_record)
-#     return 'helloworld'
 
 if __name__ == "__main__":
     db.create_all()
